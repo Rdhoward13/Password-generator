83c150148ca7847ae0f56217b8c1fb6f3dac3bc8
rachel howard <rdfields13@gmail.com>
2022-10-26 14:55:39 -0500

    completed assignment

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Password Generator Starter Code
+# Password Generator
+
+## Description
+
+This is a password generator, created with HTML, CSS, and JavaScript. This application will take in user input, and create a desired password based on length, and character types.
+
+## Links & Screenshot
+
+<img src="./assets/pwGen.png" />
--- script.js
+++ script.js
@@ -1,16 +1,45 @@
 // Assignment code here
-
+var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("")
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+var numbers = "1234567890".split("")
+var symbols = "!@#$%^&*()".split("")
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var beforeReadyPassword = [];
+  var readyPassword = ""
+  var passwordLength = prompt("How many characters would you like your password to be?\n8-128")
 
-  passwordText.value = password;
-
+  if (passwordLength > 128 || passwordLength < 8) {
+    alert("Your password must be between 8 and 128 characters.")
+    writePassword()
+  } else {
+    if(confirm("Would you like lower case letters in your password?")) {
+      beforeReadyPassword = beforeReadyPassword.concat(lowerCase) 
+    }
+    if(confirm("Would you like upper case letters in your password?")) {
+      beforeReadyPassword = beforeReadyPassword.concat(upperCase) 
+    }
+    if(confirm("Would you like numbers in your password?")) {
+      beforeReadyPassword = beforeReadyPassword.concat(numbers) 
+    }
+    if(confirm("Would you like symbols in your password?")) {
+      beforeReadyPassword = beforeReadyPassword.concat(symbols) 
+    }
+    if(beforeReadyPassword.length === 0) {
+      alert("You must have atleast one type of character to create a password.")
+      writePassword()
+    } else {
+      for (var i = 0; i < passwordLength; i++) {
+        var pickRandom = Math.floor(Math.random() * beforeReadyPassword.length)
+        readyPassword += beforeReadyPassword[pickRandom]
+      }
+    }
+  }
+document.getElementById("password").innerHTML = readyPassword
 }
 
 // Add event listener to generate button
